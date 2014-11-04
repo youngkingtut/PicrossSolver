cd57e2bb6dd3953bcda8b3b284bd27f597243558
Tristan <tristanstorz@gmail.com>
2014-11-03 23:15:22 -0700

    Adding Picross class, pretty limited in scope, just need to add in finding the vertical hints and I'll have a sturdy foundation

---
+++ Picross.py
@@ -0,0 +1,61 @@
+
+class PicrossPuzzle:
+    """
+        Picross Puzzle contains all methods for creating and
+        Translating a puzzle from hints to solution. Intialized
+        with a equal size 2d array (initially).
+    """
+    def __init__(self, puzzle):
+        try:
+            self.verify_puzzle(puzzle)
+        except NameError, error:
+            puzzle = [1]
+            print error
+
+        self.solution  = puzzle
+        self.size = len(puzzle)
+        self.calculate_hints(puzzle)
+
+    def verify_puzzle( self, puzzle ):
+        for row in puzzle:
+            if len(row) != len( puzzle ):
+                raise NameError("Invalid puzzle, setting puzzle to simple case")
+
+    def calculate_hints( self, puzzle ):
+        vertical_hints = []
+        horizontal_hints = []
+        temp_counter = 0
+
+        for row_index, row in enumerate( puzzle ):
+            temp_counter = 0
+            horizontal_hints.append([])
+
+            for column_index, column_val in enumerate( row ):
+                if column_val > 0:
+                    temp_counter += 1 ;
+
+                if column_val == 0 and temp_counter > 0:
+                    horizontal_hints[row_index].append(temp_counter)
+                    temp_counter = 0
+
+                if column_index == self.size - 1:
+                    if column_val > 0:
+                        horizontal_hints[row_index].append(temp_counter)
+                        temp_counter = 0
+
+            if len(horizontal_hints[row_index]) == 0:
+                horizontal_hints[row_index].append(0)
+
+
+        self.vertical_hints = vertical_hints
+        self.horizontal_hints = horizontal_hints
+
+
+
+
+
+
+some = PicrossPuzzle([[1,0,1],[0,1,0],[1,1,0]])
+print some.solution
+print some.horizontal_hints
+
