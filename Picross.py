1e52fb48149a7bd455b5b8563e150261f5d71b63
Tristan <tristanstorz@gmail.com>
2014-11-03 23:38:02 -0700

    added vertical hints. I didn't like the end solution for iteraating through the columns of an array. This is heavily relient on symmetry. I'd like to eventually have asymmetric 2d puzzles so this will need to be reevaluated

--- Picross.py
+++ Picross.py
@@ -1,3 +1,6 @@
+def column(array, i):
+    return [row[i] for row in array]
+
 
 class PicrossPuzzle:
     """
@@ -46,6 +49,23 @@ class PicrossPuzzle:
             if len(horizontal_hints[row_index]) == 0:
                 horizontal_hints[row_index].append(0)
 
+        for column_index in range(len(puzzle)):
+            temp_counter = 0
+            vertical_hints.append([])
+            
+            for row_index, row_val in enumerate([row[column_index] for row in puzzle]):
+
+                if row_val > 0:
+                    temp_counter += 1 ;
+
+                if row_val == 0 and temp_counter > 0:
+                    vertical_hints[column_index].append(temp_counter)
+                    temp_counter = 0
+
+                if row_index == self.size - 1:
+                    if row_val > 0:
+                        vertical_hints[column_index].append(temp_counter)
+                        temp_counter = 0
 
         self.vertical_hints = vertical_hints
         self.horizontal_hints = horizontal_hints
@@ -55,7 +75,10 @@ class PicrossPuzzle:
 
 
 
-some = PicrossPuzzle([[1,0,1],[0,1,0],[1,1,0]])
+some = PicrossPuzzle([[1,0,1],
+                      [0,1,0],
+                      [1,1,1]])
 print some.solution
 print some.horizontal_hints
+print some.vertical_hints
 
